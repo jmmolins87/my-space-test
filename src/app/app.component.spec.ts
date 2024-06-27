cb401da406b43b27c867b5c1492e4d5c29fa3100
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-06-27 18:02:18 +0200

    fix skeleto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'my-space'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-space');
   });
 
   it('should render title', () => {
