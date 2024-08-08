93734245c787f51da82ea797c85fe7c311bd90d8
Juanma Molins Cortés <juan.maria.molins@accenture.com>
2024-08-08 11:01:47 +0200

    manage sound video home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { 
+  AfterViewInit,
+  Component, 
+  ElementRef, 
+  OnInit, 
+  ViewChild 
+} from '@angular/core';
 
 
 @Component({
@@ -6,6 +12,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  public srcVideo: string = "assets/video/bg-home.mp4";
+  public isMuted: boolean = true;
+
+  @ViewChild('videoPlayer', { static: true }) 
+  public video!: ElementRef<HTMLVideoElement>;
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const videoElement = this.video.nativeElement;
+    videoElement.play().catch(error => {
+      console.error('Error playing video:', error);
+    });
+  }
+
+  manageSound() {
+    this.isMuted = !this.isMuted;
+    this.video.nativeElement.muted = this.isMuted;
+  }
 
 }
